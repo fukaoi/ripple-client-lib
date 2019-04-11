c4a9c1f9f31c4272f780208bec03c99792d2b4db
fukaoi <goodakira7@gmail.com>
2019-04-11 18:14:48 +0900

    modified

---
+++ src/multisig.js
@@ -0,0 +1,7 @@
+const RippleAPI = require('ripple-lib').RippleAPI;
+const master = 'rsxSWHhd1MhstE8BPihxfZinj5WsnmLtPa';
+const masterSecret = 'ssmx3expkpmFBhLykNTWnaYr7D1eM';
+const reg1 = 'rsxSWHhd1MhstE8BPihxfZinj5WsnmLtPa';
+const regSecret1 = 'ssmx3expkpmFBhLykNTWnaYr7D1eM';
+
+
