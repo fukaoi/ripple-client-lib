4b87b91b2458cffb697b09f18f71c9ecc7407e93
fukaoi <goodakira7@gmail.com>
2019-04-22 18:14:26 +0900

    modified

--- src/get-accountinfo.js
+++ src/get-accountinfo.js
@@ -1,19 +1,19 @@
-'use strict';
-
-const RippleAPI = require('ripple-lib').RippleAPI;
-const api = new RippleAPI({
-  server: 'wss://s2.ripple.com:443'
-});
-
-const myAddress = 'rJumr5e1HwiuV543H7bqixhtFreChWTaHH';
-
-const main = async () => {
+async function main(){
+  const myAddress = 'rJumr5e1HwiuV543H7bqixhtFreChWTaHH';
+  const RippleAPI = require('ripple-lib').RippleAPI;
+  const api = new RippleAPI({
+    server: 'wss://s2.ripple.com:443'
+  });
   await api.connect();
-  const info = await  api.getAccountInfo(myAddress);
-  console.log(info.xrpBalance);
-  return info.xrpBalance;
+  return await api.getAccountInfo(myAddress);
 }
 
-const info = main()
-console.log(info);
+async function main2() {
+  const data = await main()
+  console.log(data);
+  return data;
+}
+
+
+console.log(main2())
 
