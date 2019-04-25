f9cfb6fe4e77149e75be1956485d32614f76b76a
fukaoi <goodakira7@gmail.com>
2019-04-25 19:43:49 +0900

    Imple

--- src/create-signerlist.js
+++ src/create-signerlist.js
@@ -4,48 +4,51 @@ const RippleAPI = require('ripple-lib').RippleAPI;
 class CreateSignerList {
   constructor(srv, master_key, signer_entries) {
     this.api = new RippleAPI({server: srv});
+    this.master_key = master_key;
+    this.signer_entries = signer_entries;
   }
 
   async connect() {
     await this.api.connect();
   }
 
-  async getSequenc(address) {
+  async getSequence(address) {
     return await this.api.getAccountInfo(address)
+  }
+
+  async setupMultisig(quorum, fee) {
+    const seq = await this.getSequence(); 
+    const txJson = {
+      'Flags': 0,
+      'TransactionType': 'SignerListSet',
+      'Account': this.master_key.address,
+      'Sequence': seq,
+      'Fee': this.fee,
+      'SignerQuorum': this.quorum,
+      'SignerEntries': this.signers,
+    }    
+    return txJson;
+  }
+
+  async main(quorum, fee) {
+    try {
+      await this.connect();
+      const txJson =   await setupMultisig(quorum, fee)
+      const signedTx = await api.sign(JSON.stringify(txJson), master_key.secret)
+      const receipt =  await api.submit(signedTx.signedTransaction)
+      console.log("receipt\n", receipt)
+    } catch(e) {
+      console.error(e);
+    } finally {
+      this.api.disconnect();
+    }
   }
 }
 
-
-const fee = '10'
-const setupMultisig = async (multiSignAddress, signers, quorum) => {
-  const txJson = {
-    'Flags': 0,
-    'TransactionType': 'SignerListSet',
-    'Account': multiSignAddress.address,
-    'Sequence': await multiSignAddress.sequence(multiSignAddress.address),
-    'Fee': fee,
-    'SignerQuorum': quorum,
-    'SignerEntries': signers,
-  }    
-  return txJson;
+const quorum = process.env.QUORUM;
+const fee = 10;
+if (process.env.FEE != undefined) {
+  fee = process.env.FEE    
 }
 
-async function main() {
-  await connect()
-  const txJson = await setupMultisig(masterAddress, signerEntries, 2)
-  const signedTx = await api.sign(JSON.stringify(txJson), masterAddress.secret)
-  console.log("signedTx:\n", signedTx)
-  const receipt = await api.submit(signedTx.signedTransaction)
-  console.log("receipt\n", receipt)
-}
-
-main().then(res => {
-  console.log('## Success ##');
-  console.log(res);
-  api.disconnect();
-}).catch(error => {
-  console.log('## Error ##');
-  console.error(error);
-  api.disconnect();
-});
-
+new CreateSignerList().main(quorum, fee);
--- src/define.js
+++ src/define.js
@@ -4,6 +4,7 @@ const exec = require('child_process').exec;
 process.env.SERVER = config.get('server');
 process.env.SIGNER_ENTRIES = config.get('signer_entries');
 process.env.MASTER_KEY = config.get('master_key');
+process.env.QUORUM = config.get('quorum')
 
 exec(`node ${process.argv[2]}`, function(err, stdout, stderr){
   console.log(stdout);
