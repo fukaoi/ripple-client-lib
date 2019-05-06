a6e970ecd80ae8417df838aa541cf8a7a99760eb
fukaoi <goodakira7@gmail.com>
2019-05-06 12:11:59 +0900

    Refacotirng

--- test/address.test.js
+++ test/address.test.js
@@ -2,26 +2,17 @@ const config = require('config');
 const Address = require('../src/lib/address')
 const Define = require('./define');
 
+let address = new Address();
 
-let address;
-let faucetAddress;
-
-afterEach(() => {
-  console.log('## Call after method. ##');
-  address.disconnect();
+afterAll(async () => {
+  await address.disconnect();
 });
 
-beforeAll(async () => {
-  address = new Address();
-  const res = JSON.parse(await address.generateFaucet()); 
-  console.log(res.account.address, res.account.secret, res.balance);
-  faucetAddress = res.account.address;
-  Define.sleep(5000); 
+beforeAll(async () =>{
+  await address.connect();  
 });
 
 test('Generate new address', async () => {
-  address = new Address();
-  await address.connect();
   const created = await address.newAddress();
   expect(created.secret).not.toBeUndefined();
   expect(created.address).not.toBeUndefined();
@@ -29,8 +20,7 @@ test('Generate new address', async () => {
 
 
 test('Get seq number', async () => {
-  address = new Address();
-  await address.connect();
+  const faucetAddress = await Define.address();
   const seq = await address.getSequence(faucetAddress);
   await expect(seq).toBeGreaterThan(0);
 });
