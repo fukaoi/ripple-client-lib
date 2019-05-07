9c458760d682ca48f3b20aecc3eea60d5b63913c
fukaoi <goodakira7@gmail.com>
2019-05-07 15:33:37 +0900

    Revert file

---
+++ test/define.js
@@ -0,0 +1,19 @@
+const Address = require('../src/address');
+
+const Define = {
+  sleep: (waitMsec) => {
+    let startMsec = new Date();
+    while (new Date() - startMsec < waitMsec);
+  },
+
+  address: async () => {
+    const address = new Address();
+    const res = JSON.parse(await address.generateFaucet()); 
+    Define.sleep(5000); 
+    return res.account.address;
+  }
+};
+
+module.exports = Define;
+
+jest.setTimeout(60000); // jest timeout 60sec
