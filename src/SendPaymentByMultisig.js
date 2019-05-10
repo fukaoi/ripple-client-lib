77a8169d6ab123dd838b4d09fc077a5b5b66be65
fukaoi <goodakira7@gmail.com>
2019-05-10 16:57:39 +0900

    Fix typo, rename file name

--- src/SendPaymentByMultisig.js
+++ src/SendPaymentByMultisig.js
@@ -1,11 +1,11 @@
 const Payment = require('lib/payment');
 
-async function main(masterAddress, destionAddress, regularKeys, amount, quorum, tags, memos) {
+async function main(masterAddress, destinationAddress, regularKeys, amount, quorum, tags, memos) {
   try {
     const p = new Payment(masterAddress, regularKeys);
 
     const source = p.createSouce(amount, tags.source);
-    const destination = p.createDestination(amount, destionAddress, tags.destination);
+    const destination = p.createDestination(amount, destinationAddress, tags.destination);
     const txRaw = p.setupTransaction(source, destination, memos);
     const txJson = await p.preparePayment(txRaw, quorum);
     const regularKeySingneds = await p.setupSignerSignning(txJson);
@@ -18,7 +18,7 @@ async function main(masterAddress, destionAddress, regularKeys, amount, quorum, 
 }
 
 const masterAddress = JSON.parse(process.env.MASTER_KEY).address;
-const destionAddress = process.env.TO;
+const destinationAddress = process.env.TO;
 const quorum = parseInt(process.env.QUORUM);
 const signerLists = JSON.parse(process.env.SIGNER_LISTS);
 const regularKeys = JSON.parse(process.env.REGULAR_KEYS);
@@ -26,4 +26,4 @@ const amount = JSON.parse(process.env.AMOUNT);
 const tags = JSON.parse(process.env.TAGS);
 const memos = JSON.parse(process.env.MEMOS);
 
-main(masterAddress, destionAddress, regularKeys, amount, quorum, tags, memos);
+main(masterAddress, destinationAddress, regularKeys, amount, quorum, tags, memos);
