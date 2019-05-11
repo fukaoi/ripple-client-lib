0b5e3fd1e4876cef1c8060c5af9a455722c77acd
fukaoi <goodakira7@gmail.com>
2019-05-11 19:13:19 +0900

    Refactoring and param check

--- src/multisig.js
+++ src/multisig.js
@@ -1,37 +1,29 @@
-const Client = require("./client");
 const Address = require("./address");
-const BigNumber = require("bignumber.js");
 
 module.exports = class Multisig {
-  constructor(masterAddress, quorum) {
-    this.masterAddress = masterAddress;
-    this.quorum = quorum; // todo: Need instance param?
-    this.api = Client.instance;
+  constructor(ripplelibs) {
+    this.api = ripplelib;
   }
 
-  async setupMultisig(signerEntries) {
-    try {
-      await this.api.connect();
+  async setupMultisig(masterAddres, signerEntries, quorum, fee) {
+      if (!masterAddress || !quorum || !fee) {
+        throw new Error(`Set params(masterAddress, quorum, fee) is invalid`); 
+      }
+      const seq = await new Address().getSequence(masterAddress);
       //todo: what Flags???
-      const seq = await new Address().getSequence(this.masterAddress);
       const txjson = {
         Flags: 0,
         TransactionType: "SignerListSet",
         Account: this.masterAddress,
         Sequence: seq,
-        Fee: `${this.setupFee(signerEntries.length)}`,
-        SignerQuorum: this.quorum,
+        Fee: `${fee}`,
+        SignerQuorum: quorum,
         SignerEntries: signerEntries
       };
       return JSON.stringify(txjson);
-    } catch (e) {
-      throw new Error(e);
-    } finally {
-      await this.api.disconnect();
-    }
   }
 
-  setupSignerList(signers = [{ address: "", weight: 0 }]) {
+  createSignerList(signers = [{ address: "", weight: 0 }]) {
     let signerEntries = [];
     signers.map(signer => {
       let entry = {
@@ -42,23 +34,9 @@ module.exports = class Multisig {
     return signerEntries;
   }
 
-  setupFee() {
-    const fee = 10;
-    const x = new BigNumber(fee);
-    const y = new BigNumber(this.quorum); // todo: miss signerCounts
-    return x.times(y).toNumber();
-  }
-
   async broadCast(txjson, secret) {
-    try {
-      await this.api.connect();
-      const signedTx = await this.api.sign(txjson, secret);
-      const res = await this.api.submit(signedTx.signedTransaction);
-      return res;
-    } catch (e) {
-      throw new Error(e);
-    } finally {
-      await this.api.disconnect();
-    }
+    const signedTx = await this.api.sign(txjson, secret);
+    const res = await this.api.submit(signedTx.signedTransaction);
+    return res;
   }
 };
