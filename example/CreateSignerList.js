de453b883c16df48ae74ea32e0221978b195f6ab
fukaoi <goodakira7@gmail.com>
2019-05-12 10:52:12 +0900

    Fix code format

--- example/CreateSignerList.js
+++ example/CreateSignerList.js
@@ -1,14 +1,19 @@
 const Multisig = require("../src/multisig");
 const RippleAPI = require("ripple-lib").RippleAPI;
-const SERVER = 'wss://s.altnet.rippletest.net:51233';
+const SERVER = "wss://s.altnet.rippletest.net:51233";
 
 async function main(masterKey, quorum, fee, signerLists, regularKeys) {
-  const api = new RippleAPI({server: SERVER});
+  const api = new RippleAPI({ server: SERVER });
   try {
     await api.connect();
     const m = new Multisig(api);
     const entries = m.createSignerList(signerLists);
-    const txjson = await m.setupMultisig(masterKey.address, entries, quorum, fee);
+    const txjson = await m.setupMultisig(
+      masterKey.address,
+      entries,
+      quorum,
+      fee
+    );
     const res = await m.broadCast(txjson, masterKey.secret);
     console.log(JSON.stringify(res));
   } catch (e) {
@@ -19,32 +24,29 @@ async function main(masterKey, quorum, fee, signerLists, regularKeys) {
   }
 }
 
-const masterKey = 
-{
+const masterKey = {
   address: "raNMGRcQ7McWzXYL7LisGDPH5D5Qrtoprp",
   secret: "ssBKGd1TqzG1G1MrVMEw262ao98Sq"
 };
 
-const signerLists = 
-[
+const signerLists = [
   {
-    address:"rE4tPhwXD9PQJ1pZFrCbJphTfVKwq5aTwS",
-    weight:1
+    address: "rE4tPhwXD9PQJ1pZFrCbJphTfVKwq5aTwS",
+    weight: 1
   },
   {
-    address:"rLbTw2MioZ4spnjvQr1vTRDHaURSxtJGFK",
-    weight:1
+    address: "rLbTw2MioZ4spnjvQr1vTRDHaURSxtJGFK",
+    weight: 1
   },
   {
-    address:"rfQcZ4ia3HxFzr7m8wibceXvHBfRr94iU6",
-    weight:1
+    address: "rfQcZ4ia3HxFzr7m8wibceXvHBfRr94iU6",
+    weight: 1
   }
 ];
-  
-const regularKeys =  
-[
-  { 
-    address:"rE4tPhwXD9PQJ1pZFrCbJphTfVKwq5aTwS",
+
+const regularKeys = [
+  {
+    address: "rE4tPhwXD9PQJ1pZFrCbJphTfVKwq5aTwS",
     secret: "ssJaD4Gq2JucUJwjQm8cRafkTvVos"
   },
   {
